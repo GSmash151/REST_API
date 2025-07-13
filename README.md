2720f4d9b532f7fa7a3948c0a6dbc1cae8ccaaae
GSmash151 <ganilsmash@gmail.com>
2025-07-13 12:48:15 -0400

    employee again

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # Rest-API-# REST_API
 youtube tutorial  https://youtu.be/8d1HgJTEGe8
 
-What the fuck is wrong git 
+
--- drf_project/api/views.py
+++ drf_project/api/views.py
@@ -10,14 +10,37 @@ from rest_framework.decorators import api_view
 @api_view(['GET', 'POST'])
 # Create your views here.
 def studentsView(request):
-  if request.method == 'GET':
-    # Get all the data from the Students table
-    students = Student.objects.all()
-    serializer = StudentSerializers(students, many=True)
-    return Response(serializer.data, status=status.HTTP_200_OK)
-  elif request.method == 'POST':  
-    serializer = StudentSerializers(data=request.data)
-    if serializer.is_valid():
-      serializer.save()
-      return Response(serializer.data, status=status.HTTP_201_CREATED)
-    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    if request.method == 'GET':
+        # Get all the data from the Students table
+        students = Student.objects.all()
+        serializer = StudentSerializers(students, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    elif request.method == 'POST':  
+        serializer = StudentSerializers(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(["GET", "PUT", "DELETE"])
+def studentDetailView(request, pk):
+    try:
+        student = Student.objects.get(pk=pk)
+    except Student.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    if request.method == "GET":
+        serializer = StudentSerializers(student)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    elif request.method == "PUT":
+        serializer = StudentSerializers(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    elif request.method == 'DELETE':
+      student.delete()
+      return Response(status=status.HTTP_204_NO_CONTENT)
