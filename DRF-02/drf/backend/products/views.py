69d3f726d02daf8c6ff9799d99e4b265f7d90c2b
GSmash151 <ganilsmash@gmail.com>
2025-07-15 18:10:13 -0400

    createAPIView

--- DRF-02/drf/backend/products/views.py
+++ DRF-02/drf/backend/products/views.py
@@ -1,6 +1,21 @@
+from itertools import product
 from rest_framework import generics
 from .models import Product
 from .serializer import ProductSerializer
+
+class ProductCreateAPIView(generics.CreateAPIView):
+  queryset = Product.objects.all()
+  serializer_class = ProductSerializer
+
+  def perform_create(self, serializer):
+    print(serializer.validated_data)
+    title = serializer.validated_data.get('title')    
+    content = serializer.validated_data.get('content') or title
+    if content is None:
+      content = title   
+    serializer.save(content=content)
+
+product_create_view = ProductCreateAPIView.as_view()
 
 class ProductDetailAPIView(generics.RetrieveAPIView):
   queryset = Product.objects.all()
