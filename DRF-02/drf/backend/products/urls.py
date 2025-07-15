35f9fb62a1e554880de90b293faac9d2349b5f77
GSmash151 <ganilsmash@gmail.com>
2025-07-15 19:57:59 -0400

    alt_view & not_found

--- DRF-02/drf/backend/products/urls.py
+++ DRF-02/drf/backend/products/urls.py
@@ -3,6 +3,8 @@ from . import views
 
 urlpatterns = [
  # path('', views.product_create_view),
-  path('', views.product_list_create_view),
-  path('<int:pk>/', views.ProductDetailAPIView.as_view())
+ # path('', views.product_list_create_view),
+  path('', views.product_alt_view),
+  path('<int:pk>/', views.product_alt_view)
+  # path('<int:pk>/', views.ProductDetailAPIView.as_view())
 ]
--- DRF-02/drf/backend/products/views.py
+++ DRF-02/drf/backend/products/views.py
@@ -1,10 +1,14 @@
-from itertools import product
+from rest_framework import status
 from rest_framework import generics
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+# from django.http import Http404
+from django.shortcuts import get_object_or_404
 from .models import Product
 from .serializer import ProductSerializer
 
 class ProductListCreateAPIView(generics.ListCreateAPIView):
-  queryset = Product.objects.all()
+  qs = Product.objects.all()
   serializer_class = ProductSerializer
 
   def perform_create(self, serializer):
@@ -18,32 +22,59 @@ class ProductListCreateAPIView(generics.ListCreateAPIView):
 product_list_create_view = ProductListCreateAPIView.as_view()
 
 
-# class ProductCreateAPIView(generics.CreateAPIView):
-#   queryset = Product.objects.all()
-#   serializer_class = ProductSerializer
+class ProductCreateAPIView(generics.CreateAPIView):
+  qs = Product.objects.all()
+  serializer_class = ProductSerializer
 
-#   def perform_create(self, serializer):
-#     print(serializer.validated_data)
-#     title = serializer.validated_data.get('title')    
-#     content = serializer.validated_data.get('content') or title
-#     if content is None:
-#       content = title   
-#     serializer.save(content=content)
+  def perform_create(self, serializer):
+    print(serializer.validated_data)
+    title = serializer.validated_data.get('title')    
+    content = serializer.validated_data.get('content') or title
+    if content is None:
+      content = title   
+    serializer.save(content=content)
 
-# product_create_view = ProductCreateAPIView.as_view()
+product_create_view = ProductCreateAPIView.as_view()
 
 class ProductDetailAPIView(generics.RetrieveAPIView):
-  queryset = Product.objects.all()
+  qs = Product.objects.all()
   serializer_class = ProductSerializer
 
 product_detail_view = ProductDetailAPIView.as_view()
 
 
-class ProductListAPIView(generics.ListAPIView):
-  queryset = Product.objects.all()
-  serializer_class = ProductSerializer
+# class ProductListAPIView(generics.ListAPIView):
+#   qs = Product.objects.all()
+#   serializer_class = ProductSerializer
 
-product_List_view = ProductListAPIView.as_view()
+# product_List_view = ProductListAPIView.as_view()
+
+@api_view(["GET", "POST"])
+def product_alt_view(request, pk=None, *args, **kwargs):
+  method = request.method 
+
+  if method == "GET":
+    if pk is not None:
+        # detail view
+        obj = get_object_or_404(Product, pk=pk)
+        data = ProductSerializer(obj, many=False).data
+        return Response(data)
+    
+    # list view
+    qs = Product.objects.all()
+    data = ProductSerializer(qs, many=True).data
+    return Response(data)
+  if method == "POST":
+    # create an item 
+     serializer = ProductSerializer(data=request.data)
+     if serializer.is_valid(raise_exception=True):
+        title = serializer.validated_data.get('title')    
+        content = serializer.validated_data.get('content') or title
+        if content is None:
+          content = title   
+          serializer.save(content=content)
+        return Response(serializer.data)
+  return RecursionError({"invalid": "not good data"}, status=status.HTTP_400_BAD_REQUEST)
 
 
 
