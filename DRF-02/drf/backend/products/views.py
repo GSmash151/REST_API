0e351188b289416e306df7259effe0fb75d4beb4
GSmash151 <ganilsmash@gmail.com>
2025-07-15 18:56:24 -0400

    ListCreateAPIView

--- DRF-02/drf/backend/products/views.py
+++ DRF-02/drf/backend/products/views.py
@@ -3,7 +3,7 @@ from rest_framework import generics
 from .models import Product
 from .serializer import ProductSerializer
 
-class ProductCreateAPIView(generics.CreateAPIView):
+class ProductListCreateAPIView(generics.ListCreateAPIView):
   queryset = Product.objects.all()
   serializer_class = ProductSerializer
 
@@ -15,11 +15,35 @@ class ProductCreateAPIView(generics.CreateAPIView):
       content = title   
     serializer.save(content=content)
 
-product_create_view = ProductCreateAPIView.as_view()
+product_list_create_view = ProductListCreateAPIView.as_view()
+
+
+# class ProductCreateAPIView(generics.CreateAPIView):
+#   queryset = Product.objects.all()
+#   serializer_class = ProductSerializer
+
+#   def perform_create(self, serializer):
+#     print(serializer.validated_data)
+#     title = serializer.validated_data.get('title')    
+#     content = serializer.validated_data.get('content') or title
+#     if content is None:
+#       content = title   
+#     serializer.save(content=content)
+
+# product_create_view = ProductCreateAPIView.as_view()
 
 class ProductDetailAPIView(generics.RetrieveAPIView):
   queryset = Product.objects.all()
   serializer_class = ProductSerializer
+
+product_detail_view = ProductDetailAPIView.as_view()
+
+
+class ProductListAPIView(generics.ListAPIView):
+  queryset = Product.objects.all()
+  serializer_class = ProductSerializer
+
+product_List_view = ProductListAPIView.as_view()
 
 
 
