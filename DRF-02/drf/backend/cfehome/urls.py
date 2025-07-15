529003c0f38d4348dc5a746604558bf7d5af4cdc
GSmash151 <ganilsmash@gmail.com>
2025-07-15 17:25:37 -0400

    rest framework generics api_view

--- DRF-02/drf/backend/cfehome/urls.py
+++ DRF-02/drf/backend/cfehome/urls.py
@@ -1,9 +1,9 @@
-from xml.etree.ElementInclude import include
 from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
-    path('api-auth', include('rest_framework.urls')),
+    path('api/products/', include('products.urls')),
+    #path('api-auth', include('rest_framework.urls')),
 ]
