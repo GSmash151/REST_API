8a4ad057f71d17a82df3ce5386be58f7f0c512f6
GSmash151 <ganilsmash@gmail.com>
2025-07-16 11:54:26 -0400

    products update

--- DRF-02/drf/backend/products/urls.py
+++ DRF-02/drf/backend/products/urls.py
@@ -3,8 +3,11 @@ from . import views
 
 urlpatterns = [
  # path('', views.product_create_view),
- # path('', views.product_list_create_view),
-  path('', views.product_alt_view),
-  path('<int:pk>/', views.product_alt_view)
-  # path('<int:pk>/', views.ProductDetailAPIView.as_view())
+   path('', views.product_list_create_view),
+  # path('', views.product_alt_view),
+  # path('<int:pk>/', views.product_alt_view)
+   path('<int:pk>/', views.ProductDetailAPIView.as_view())
 ]
+
+
+# (https://youtu.be/c708Nf0cHrs?t=6914)
